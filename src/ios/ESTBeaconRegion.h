8df3c9e1be8cf6685961f42d6ba86d5a64275063
Dustin <dustin.filip@gmail.com>
2014-09-27 14:58:46 -0400

    updated library and headers

--- src/ios/ESTBeaconRegion.h
+++ src/ios/ESTBeaconRegion.h
@@ -2,7 +2,7 @@
 //  ESTBeaconRegion.h
 //  EstimoteSDK
 //
-//  Version : 1.2.0
+//  Version: 2.1.5
 //  Created by Marcin Klimek on 9/21/13.
 //  Copyright (c) 2013 Estimote. All rights reserved.
 //
@@ -10,61 +10,22 @@
 #import <Foundation/Foundation.h>
 #import <CoreLocation/CoreLocation.h>
 
-/*
- *  ESTBeaconMajorValue
- *
- *  Discussion:
- *    Type represents the most significant value in a beacon.
- *
- */
-typedef uint16_t ESTBeaconMajorValue;
-
-/*
- *  ESTBeaconMinorValue
- *
- *  Discussion:
- *    Type represents the least significant value in a beacon.
- *
- */
-typedef uint16_t ESTBeaconMinorValue;
-
-
 /**
- 
- A ESTBeaconRegion object defines a type of region that is based on the device’s proximity to a Bluetooth beacon, as opposed to a geographic location. A beacon region looks for devices whose identifying information matches the information you provide. When that device comes in range, the region triggers the delivery of an appropriate notification.
- 
- You can monitor beacon regions in two ways. To receive notifications when a device enters or exits the vicinity of a beacon, use the startMonitoringForRegion: method of your location manager object. While a beacon is in range, you can also call the startRangingBeaconsInRegion: method to begin receiving notifications when the relative distance to the beacon changes.
- 
- ESTBeaconRegion extends basic CLBeaconRegion Core Location object, allowing to directly initialize region that is supported by Estimote Cloud platform.
- 
+ * An ESTBeaconRegion object defines a single beacon or a group of beacons that can be used with ESTBeaconManager to range, monitor and discover beacons.
+ *
+ * There are three ways to define a region:
+ *
+ * 1. By providing proximityUUID, major and minor - i.e. initWithProximityUUID:major:minor:identifier: - this defines a region consisting of a single beacon that matches all three of these properties.
+ *
+ * 2. By providing proximityUUID and major - i.e. initWithProximityUUID:major:identifier: - this defines a region that can consist of multiple beacons, as long as their proximityUUID and major match those of the region.
+ *
+ * 3. By providing proximityUUID only - i.e. initWithProximityUUID:identifier: - this defines a region that can consist of multiple beacons, as long as their proximityUUID matches this of the region.
+ *
+ * For more details, consult Apple's CLBeaconRegion documentation:
+ *
+ * https://developer.apple.com/library/ios/documentation/CoreLocation/Reference/CLBeaconRegion_class/Reference/Reference.html
  */
+
 @interface ESTBeaconRegion : CLBeaconRegion
-
-/**
- * Initialize a Estimote beacon region. Major and minor values will be wildcarded.
- *
- * @param identifier Region identifier
- * @return Initialized ESTBeaconRegion object
- **/
-- (id)initRegionWithIdentifier:(NSString *)identifier;
-
-/**
- * Initialize a Estimote beacon region with major value. Minor value will be wildcarded.
- *
- * @param major minor location value
- * @param identifier Region identifier
- * @return Initialized ESTBeaconRegion object
- **/
-- (id)initRegionWithMajor:(ESTBeaconMajorValue)major identifier:(NSString *)identifier;
-
-/**
- * Initialize a Estimote beacon region identified by a major and minor values.
- *
- * @param major minor location value
- * @param minor minor location value
- * @param identifier Region identifier
- * @return Initialized ESTBeaconRegion object
- **/
-- (id)initRegionWithMajor:(ESTBeaconMajorValue)major minor:(ESTBeaconMinorValue)minor identifier:(NSString *)identifier;
 
 @end
